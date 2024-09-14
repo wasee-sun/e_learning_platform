3526bd2f7fe5ddca7c7439b118e1ebf71c1b9f6a
wasee-sun <revengersun@gmail.com>
2024-09-15 04:47:29 +0600

    Fourth Commit

--- codextra/admin.py
+++ codextra/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+import codextra.models as models
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(models.User)
+admin.site.register(models.Student)
+admin.site.register(models.Instructor)
+admin.site.register(models.Category)
+admin.site.register(models.Course)
+admin.site.register(models.Cart)
--- codextra/models.py
+++ codextra/models.py
@@ -20,3 +20,65 @@ class User(models.Model):
     def __str__(self):
         return f'{self.f_name} {self.l_name} ({self.username})'
     
+class Student(User):
+    level = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.f_name} {self.l_name}"
+
+class Instructor(User):
+    bank_account = models.IntegerField()
+    routing_no = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.f_name} {self.l_name}"
+    
+class Category(models.Model):
+    cat_id = models.AutoField(primary_key=True)
+    cat_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.cat_name
+    
+class Course(models.Model):
+    DIFFICULTY = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    ]
+    
+    course_id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=100)
+    difficulty = models.CharField(
+        max_length=15,
+        choices=DIFFICULTY,
+        default='beginner',
+    )
+    price = models.FloatField(default=0.00)
+    description = models.CharField(max_length=500)
+    inst_user_name = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    cat_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.course_name
+    
+# class course_material(models.Model):
+#     TYPE = [
+#         ('pdf', 'PDF'),
+#         ('video', 'Video'),
+#     ]
+    
+#     course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+#     material_type = models.CharField(
+#         max_length=5,
+#         choices=TYPE,
+#         default='pdf',
+#     )
+#     file_loc = models.
+    
+#     def __str__(self):
+#         return self.material_name
+
+class Cart(models.Model):
+    s_user_name = models.ForeignKey(Student, on_delete=models.CASCADE)
+    order_id = models.AutoField(primary_key=True)
