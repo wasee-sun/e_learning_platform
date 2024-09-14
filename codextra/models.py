cb821cd46eea0709050e97c8431b80997c0464b7
wasee-sun <revengersun@gmail.com>
2024-09-15 03:29:36 +0600

    Second Commit

--- codextra/models.py
+++ codextra/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    f_name = models.CharField(max_length=50, default='First name')
+    l_name = models.CharField(max_length=50, default='Last name')
+    email = models.EmailField(max_length=100, default='noreply@example.com')
+    
+    def __str__(self) -> str:
+        return f"{self.user_name}"
