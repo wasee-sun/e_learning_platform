d94341415ef7d6ede6e8242cdb96e40fc3e01eea
wasee-sun <revengersun@gmail.com>
2024-09-18 21:20:28 +0600

    Eighth final database commit Co-authored-by: arianasuha <arianasuha@users.noreply.github.com>

--- codextra/models.py
+++ codextra/models.py
@@ -50,6 +50,7 @@ class Course(models.Model):
     
     course_id = models.AutoField(primary_key=True)
     course_name = models.CharField(max_length=100)
+    course_img = models.ImageField(upload_to='course_images/', blank=True, null=True)
     difficulty = models.CharField(
         max_length=15,
         choices=DIFFICULTY,
