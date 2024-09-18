57d2b614f82bca47a79c5f0a040745027ad7491d
wasee-sun <revengersun@gmail.com>
2024-09-19 03:26:46 +0600

    Twelveth commit
    Co-authored-by: SharminAnanna <SharminAnanna@users.noreply.github.com>
    Co-authored-by: Abdullahis Sany <saany007@users.noreply.github.com>
    Co-authored-by: Rasif Yumn <RasifYumn@users.noreply.github.com>

--- codextra/urls.py
+++ codextra/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home-codextra"),
+    path("contact-us/", views.contact, name="contact-codextra"),
+    path("about/", views.about, name="about-codextra"),
+    path("terms-conditions/", views.terms_conditions, name="terms-conditions-codextra"),
+    path("login/", views.login_view, name="login-codextra"),
+    path("signup/", views.signup_view, name="signup-codextra"),
 ]
--- codextra/views.py
+++ codextra/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+import codextra.models as models
 
 # Create your views here.
 def home(request):
-    return render(request, 'codextra/home.html', {})
+    courses = models.Course.objects.all()
+    categories = models.Category.objects.all()
+    return render(request, 'codextra/home.html', {
+        "courses": courses,
+        "categories": categories
+    })
+    
+def contact(request):
+    return render(request, 'codextra/contact.html')
+
+def about(request):
+    return render(request, 'codextra/about.html')
+
+def terms_conditions(request):
+    return render(request, 'codextra/terms_conditions.html')
+
+def login_view(request):
+    pass
+
+def signup_view(request):
+    pass
